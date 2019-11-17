d5e30f3058d691dfb5fdbd824582a798fa7f7baa
AndrUsM <oksud22@gmail.com>
2019-11-18 01:05:26 +0200

    main task

---
+++ app/javascript/original/main.js
@@ -0,0 +1,102 @@
+const arrays = {}
+arrays.sum = (array) => {
+    return array.reduce((total, value) => {
+        return (total + value)
+    })
+}
+// Min
+arrays.min = (array) => {
+    let min = array[0]
+    for(let i = 0; i < array.length; i++){
+        let buffer = array[i]
+        min = (buffer < min) ? buffer : min
+    }
+    return min
+}
+// Max
+arrays.max = (array) => {
+    let max = array[0]
+    for(let i = 0; i < array.length; i++){
+        let buffer = array[i]
+        max = (buffer > max) ? buffer : max
+    }
+    return max
+}
+// Replace methods
+arrays.replace = {}
+arrays.replace.min = (array, value) => {
+    let index = array.indexOf(arrays.min(array))
+    array[index] = value
+    let raport = {
+        old: [arrays.min(array), index],
+        new: [value]
+    }
+    return raport
+}
+arrays.replace.max = (array, value) => {
+    let index = array.indexOf(arrays.max(array))
+    array[index] = value
+    let raport = {
+        old: [arrays.max(array), index],
+        new: [value]
+    }
+    return raport
+}
+// Function with callback in argument
+const func_with_callback = (object) => {
+    let data = {
+        key: [],
+        value: []
+    }
+    Object.entries(object).forEach(([key, value]) => {
+        data.key.push(key);
+        data.value.push(value)
+    })
+    return data
+}
+// Function that replace all number dividing three with foo
+const mutches = {}
+mutches.find = (str1, str2) => {
+    // Str to Lowercase
+    str1 = str1.toLowerCase()
+    str2 = str2.toLowerCase()
+    // Split array and remove repeated elements
+    let data = {
+        str_1: mutches.unique(str1),
+        str_2: mutches.unique(str2)
+    }
+    let index_of_mutches = 0
+    // Get longer array
+    let longer_array = data.str_1.length > data.str_2.length? data.str_1 : data.str_2;
+    let shorter_array = data.str_1.length < data.str_2.length? data.str_1 : data.str_2;
+    for(let i in longer_array){
+        if(longer_array.includes(shorter_array[i])){
+            index_of_mutches++
+            console.log(`${longer_array[i]}:${shorter_array[i]}, ${index_of_mutches}`)
+        }
+    }
+    // Get length of longer string
+    let length = data.str_1.length > data.str_2.length ? data.str_1.length : data.str_2.length
+    return `${(index_of_mutches/length).toPrecision(3) * 100}%`
+}
+mutches.unique = (array) => {
+    let seen = {};
+    return array.split('').filter(function(item) {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
+}
+// Replace number: foo, bar, foobar
+const replace = {}
+replace.replace = (array, key) => {
+    switch (key) {
+        case 'foo':
+            replace.additional.foo(array)
+            break;
+        case 'foo':
+            replace.additional.bar(array)
+            break;
+    
+        default:
+            break;
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const sass = require("gulp-sass");
+const gulp = require("gulp");
+const uglify = require('gulp-uglify');
+const browser_sync = require('browser-sync');
+const concat_css = require('gulp-concat-css');
+
+gulp.task('scss', () => {
+    return gulp.src(
+        'app/stylesheet/scss'
+    ).pipe(sass())
+    .pipe(gulp.dest('app/stylesheet/css'))
+    .pipe()
+    .pipe(browser_sync.reload({stream: true}))
+})
+gulp.task('html', () => {
+     return gulp.src('app/view/*.html')
+            .pipe(browser_sync.reload({stream: true}))
+})
+gulp.task('js', () => {
+    return gulp.src('app/javascript/original/**/*.js')
+            .pipe(uglify())
+            .pipe(gulp.dest('app/javascript/min'))
+            .pipe(browser_sync.reload({stream: true}))
+})
+gulp.task('watch', () => {
+    gulp.watch('app/scss', gulp.parallel('scss'))
+    gulp.watch('app/view/*.html', gulp.parallel('html'))
+    gulp.watch('app/javascript/original', gulp.parallel('js'))
+})
+gulp.task('browser-sync', () => {
+    browser_sync.init({
+        server: {
+            baseDir: 'app/',
+            directory: true
+        },
+        open: false,
+        port: 3000
+    })
+})
+gulp.task('run', gulp.parallel('scss', 'browser-sync', 'watch'))
